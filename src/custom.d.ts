f8831eb1af6cd4437b8dd3f04e10a00277842dd2
misteryfors <artem.novickov@mail.ru>
2025-01-25 10:32:09 +0300

    Updates

--- src/custom.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.svg" {
-  import { FC, SVGProps } from "react";
-  const ReactComponent: FC<SVGProps<SVGSVGElement>>;
-  export { ReactComponent };
-}
--- src/pages/Home/List/Placeholder/Placeholder.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './Placeholder.module.scss';
-
-const Placeholder: React.FC = () => <li className={styles['placeholder']}>Placeholder</li>;
-
-export default Placeholder;
